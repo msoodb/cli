6ae7406580c60267b762dd05532aacbe83ff9bdd
msoodb <masoud@.bolhassani@gmail.com>
2020-05-02 14:32:45 +0300

    copyright

--- cli.c
+++ cli.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2020 cmock
+ * Copyright (c) 2020-2020 msoodb.org
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
