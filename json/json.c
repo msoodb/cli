2d3e4c1b9b734547ce31cc19716d3bfdf33197ae
msoodb <c.msoodb@gmail.com>
2020-05-22 23:07:14 +0300

    lexer json

--- json/json.c
+++ json/json.c
@@ -1,26 +1,34 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stack.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
 
-
-/*
- * stack node
- */
-struct node
+typedef struct __json__
 {
-	char data;	
-	struct node *next;
-};
-
-typedef struct _json
-{
-	struct _json *prev;
-	struct _json *child;
-	struct _json *next;	
+	struct __json__ *prev;	
+	struct __json__ *child;
+	struct __json__ *next;
+	char *key;
+	char *value;
+	int type;
 } JSON;
-
 
 typedef struct _key_value
 {
@@ -29,40 +37,77 @@ typedef struct _key_value
 } KEY_VALUE;
 
 
-/* 
- * stack push data 
-*/
-void push(struct node **ptrhead, char data)
+
+#define IS_EMPTY_LIST(lsit)  ((list) == NULL)
+
+JSON *init_json_node(char *key, char *value, int type)
 {
-	struct node *new = (struct node *)malloc(sizeof(struct node));
-	if (new == NULL) {
-		printf("out of memory!\n");
-		return;
-	}
-	new->data = data;
-	new->next = (*ptrhead);
-
-	(*ptrhead) = new;
-}
-
-/* 
- * stack pop data 
-*/
-char pop(struct node **ptrhead)
-{
-	char data;
-	struct node *top;
-	if (*ptrhead == NULL) {
-		return 0;
-	}
-
-	top = *ptrhead;	
-	data = top->data;	
-	*ptrhead = top->next;
-	free(top);
+	JSON *new = (JSON*) malloc(sizeof(JSON) * 1);
+	if (new == NULL) return NULL;
 	
-	return data;
+	new->prev = NULL;
+	new->child = NULL;
+	new->next = NULL;
+
+	new->key = key;
+	new->value = value;
+	new->type = type;
+
+	return new;
 }
+
+void json_push(JSON **list, JSON *node)
+{
+	JSON *current = *list;
+	*list = node;
+
+	node->next = current;
+	if (current) {
+		current->prev = node;
+	}
+}
+
+JSON *json_pop(JSON **list)
+{
+	if (*list == NULL) return NULL;
+	
+	JSON *current = *list;
+
+	*list = (*list)->next;
+	if (*list) {
+		(*list)->prev = NULL;
+	}
+	
+	return current;
+}
+
+/*void print(JSON *list)
+{
+	int node_number = 0;
+	while (list != NULL) {
+
+		printf("\t\t%s\n", " ---------------------");
+		printf("%8d", list);
+		printf("\t%s %8d", "|", list->prev);
+		printf("%s", " | ");
+		printf("%8d %s\n", list->next, "|");
+		printf("\t\t%s\n\n", " ---------------------");		
+
+		list = list->next;
+	}	
+	}*/
+
+void print(JSON *list)
+{
+	int node_number = 0;
+	while (list != NULL) {
+		printf("%s:", list->key);
+		printf("%s\n", list->value);
+		list = list->next;
+	}	
+}
+
+
 
 
 /* 
@@ -84,14 +129,14 @@ bool parenthes_balanced(char *exp)
 { 
 	int i = 0; 
  
-	struct node *stack = NULL; 
+	struct stack_node *stack = NULL; 
   
 	while (exp[i]){ 
 		if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') 
-			push(&stack, exp[i]); 
+			stack_push(&stack, exp[i]); 
   
 		if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']'){ 
-			if (!is_match(pop(&stack), exp[i])) return 0;
+			if (!is_match(stack_pop(&stack), exp[i])) return 0;
 		} 
 		i++; 
 	} 
@@ -101,17 +146,16 @@ bool parenthes_balanced(char *exp)
 	return 0;
 }
 
-
-KEY_VALUE *key_valuer(char *chunk)
+JSON *lexer(char *chunk)
 {
-	KEY_VALUE *kval;
+	JSON *node;
 	size_t step;
-	
-	kval = malloc(sizeof(KEY_VALUE) * 1);
-	if (kval == NULL) {
-		return NULL;
-	}
 
+	char *key;
+	char *value;
+	int type;
+	
+	node = NULL;
 	
 	/* 
 	 * key 
@@ -122,13 +166,17 @@ KEY_VALUE *key_valuer(char *chunk)
 
 	step = strcspn(chunk, "\"\0");
 
-	kval->key = malloc(sizeof(char) * (step + 1));	
-	if (kval->key == NULL) return NULL;
+	key = malloc(sizeof(char) * (step + 1));	
+	if (key == NULL) return NULL;
 	
-	memcpy(kval->key, chunk, step);
-	kval->key[step] = '\0';
+	memcpy(key, chunk, step);
+	key[step] = '\0';
 	chunk += step + 1;
 
+	/*
+	 * type
+	 */
+	type = 0;
 		
 	/* 
 	 * value 
@@ -136,20 +184,24 @@ KEY_VALUE *key_valuer(char *chunk)
 	while(isspace((unsigned char)*chunk) || *chunk == ':' || *chunk == '"') chunk++;
 		
 	step = strcspn(chunk, "\",\0");
-	kval->value = malloc(sizeof(char) * (step + 1));
-	if (kval->value == NULL) return NULL;
+	value = malloc(sizeof(char) * (step + 1));
+	if (value == NULL) return NULL;
 	
-	memcpy(kval->value, chunk, step);
-	kval->value[step] = '\0';
+	memcpy(value, chunk, step);
+	value[step] = '\0';
 
-	return kval;
+	node = init_json_node(key, value, type);
+
+	return node;
 }
 
-void parse(const char *stream)
+JSON *parse(const char *stream)
 {
 	char *chunk;
 	size_t step;
-
+	JSON *list = NULL;
+	JSON *node = NULL;
+	
 	chunk = NULL;
 	step = 0;
 	
@@ -163,20 +215,12 @@ void parse(const char *stream)
 		chunk[step] = '\0';
 		
 		stream += step + 1;
-		
-		
-		KEY_VALUE *kval;
-		kval = key_valuer(chunk);
-		if (kval != NULL) {
-			printf("%s", kval->key);
-			if (kval->value != NULL) {
-				printf(": %s", kval->value);
-			}
-			printf("\n");
-		}
+				
+		node = lexer(chunk);
+		json_push(&list, node);
 	}
 
-	return;
+	return list;
 }
 
 char *read_file(const char *file)
@@ -206,15 +250,21 @@ char *read_file(const char *file)
 int main()
 {
 	char *stream;
-	stream = read_file("package.json");
+	stream = read_file("package-min.json");
 
 	//parse(stream);
 
-	char *exp = "{()}[fghfgh sdgf(gfh{d}fgh)sdfg[]]"; 
+	/*char *exp = "{()}[fghfgh sdgf(gfh{d}fgh)sdfg[]]"; 
 	if (parenthes_balanced(stream)) 
 		printf("Balanced \n"); 
 	else
 		printf("Not Balanced \n");   
+	*/
+
+	JSON *list = NULL;
+	
+	list = parse(stream);
+	print(list);
 
 	return 0;
 }
